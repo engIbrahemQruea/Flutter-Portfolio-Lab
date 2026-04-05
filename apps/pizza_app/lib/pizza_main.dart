523a2a50e9ac99ec11f835b8b40e9239f1afaebf
engIbrahemQruea <mmany6186@gmail.com>
2026-04-05 16:36:12 +0300

    add:  Root widget of the application.  Configures the [MaterialApp] theme and provides the [PizzaProvider]  to the [PizzaOrderScreen] scope using Provider.

--- apps/pizza_app/lib/pizza_main.dart
+++ apps/pizza_app/lib/pizza_main.dart
@@ -3,13 +3,14 @@ import 'package:pizza_app/features/screens/home/logic/pizza_provider.dart';
 import 'package:pizza_app/features/screens/home/pizza_order_screen.dart';
 import 'package:provider/provider.dart';
 
+
 class PizzaMain extends StatelessWidget {
   const PizzaMain({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pizza App',
+      title: 'Pizza App', 
       debugShowCheckedModeBanner: false,
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: ChangeNotifierProvider(
