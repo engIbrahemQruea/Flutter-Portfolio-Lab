c734a020d06d1d120ff4efc92ef3a105c41c7659
engIbrahemQruea <mmany6186@gmail.com>
2026-05-21 17:32:17 +0300

    ref: structure project

---
+++ learning_resources/advanced/lib/main_app.dart
@@ -0,0 +1,15 @@
+import 'package:advanced/001_send_data_between_forms/screen_one.dart';
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const ScreenOne(),
+    );
+  }
+}
