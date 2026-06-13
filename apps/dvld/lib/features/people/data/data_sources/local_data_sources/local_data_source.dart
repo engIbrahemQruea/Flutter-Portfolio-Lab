b7fbb366ececadd27533192846c95fa7fdca74cc
engIbrahemQruea <mmany6186@gmail.com>
2026-06-13 23:02:48 +0300

    feat: implement DatabaseHelper for managing local database operations and seeding data

---
+++ apps/dvld/lib/features/people/data/data_sources/local_data_sources/local_data_source.dart
@@ -0,0 +1,53 @@
+import 'package:dvld/core/database/init_table.dart';
+import 'package:dvld/features/people/data/models/people_models.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+
+  DatabaseHelper._internal();
+
+  static Database? _database;
+  final String _databaseName = 'dvld_database.db';
+
+  factory DatabaseHelper() => _instance;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final path = await getDatabasesPath() + _databaseName;
+    return await openDatabase(
+      path,
+      version: 1,
+      onConfigure: (db) async {
+        await db.execute('PRAGMA foreign_keys = ON');
+      },
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> close() async {
+    final db = _database;
+    if (db != null) {
+      await db.close();
+      _database = null;
+    }
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute(CountryTable.createTableQuery);
+    await db.execute(CountryTable.seedCountriesQuery);
+    await db.execute(PersonTable.createTableQuery);
+    await db.execute(PersonTable.seedPeopleQuery);
+  }
+
+  Future<List<PeopleModels>> getListPeople() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(PersonTable.tableName);
+    return maps.map((e) => PeopleModels.fromJson(e)).toList();
+  }
+}
