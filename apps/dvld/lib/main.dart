1f8e264abd5ddd76ba748ea567a5b5897370d43b
engIbrahemQruea <mmany6186@gmail.com>
2026-06-13 23:05:53 +0300

    feat: update main function for platform-specific database initialization and modify pubspec.yaml for new dependencies

--- apps/dvld/lib/main.dart
+++ apps/dvld/lib/main.dart
@@ -1,6 +1,17 @@
+import 'dart:io';
+
+import 'package:dvld/core/di/dependency_injection.dart';
 import 'package:dvld/dvld_app.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+  await setupGetIt();
   runApp(const DvldApp());
 }
