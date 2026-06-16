36c648a9b5bdee3c25c61588d6a20753b9313f0b
engIbrahemQruea <mmany6186@gmail.com>
2026-06-16 23:06:05 +0300

    feat: add Filter query methods to fetch people by ID, national number, and first name in DatabaseHelper

--- apps/dvld/lib/features/people/data/data_sources/local_data_sources/local_data_source.dart
+++ apps/dvld/lib/features/people/data/data_sources/local_data_sources/local_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:dvld/core/database/init_table.dart';
 import 'package:dvld/features/people/data/models/people_models.dart';
+import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
@@ -47,7 +48,54 @@ class DatabaseHelper {
 
   Future<List<PeopleModels>> getListPeople() async {
     final db = await database;
-    final List<Map<String, dynamic>> maps = await db.query(PersonTable.tableName);
+    final List<Map<String, dynamic>> maps = await db.query(
+      PersonTable.tableName,
+    );
     return maps.map((e) => PeopleModels.fromJson(e)).toList();
+  }
+
+  Future<List<PeopleModels?>> getPeopleById({required int personID}) async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      PersonTable.tableName,
+      where: '${PersonTable.colId} Like ?',
+      whereArgs: ['%$personID%'],
+    );
+    return maps.isNotEmpty
+        ? maps.map((e) => PeopleModels.fromJson(e)).toList()
+        : [null];
+  }
+
+  Future<List<PeopleModels?>> getPeopleByNationalNo({
+    required String nationalNo,
+  }) async {
+    try {
+      final db = await database;
+      final List<Map<String, dynamic>> maps = await db.query(
+        PersonTable.tableName,
+        where: '${PersonTable.colNationalNo} LIKE ?',
+        whereArgs: ['%$nationalNo%'],
+      );
+      return maps.isNotEmpty
+          ? maps.map((e) => PeopleModels.fromJson(e)).toList()
+          : [null];
+    } on Exception catch (e) {
+      debugPrint(e.toString());
+      return [null];
+    }
+  }
+
+  Future<List<PeopleModels?>> getPeopleByFirstName({
+    required String firstName,
+  }) async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      PersonTable.tableName,
+      where: '${PersonTable.colFirstName} Like ?',
+      whereArgs: ['%$firstName%'],
+    );
+    return maps.isNotEmpty
+        ? maps.map((e) => PeopleModels.fromJson(e)).toList()
+        : [null];
   }
 }
