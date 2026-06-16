c5361c4468994774ceb87fae25ff96f4ea1ec73f
engIbrahemQruea <mmany6186@gmail.com>
2026-06-16 23:07:08 +0300

    feat: add Filter methods to fetch people by ID, national number, and first name in PeopleRepos

--- apps/dvld/lib/features/people/domain/repos/people_repos.dart
+++ apps/dvld/lib/features/people/domain/repos/people_repos.dart
@@ -4,4 +4,13 @@ import 'package:dvld/features/people/domain/entities/people_entity.dart';
 
 abstract class PeopleRepos {
   Future<Either<Failure, List<PeopleEntity>>> getListPeople();
+  Future<Either<Failure, List<PeopleEntity?>>> getPeopleById({
+    required int personID,
+  });
+  Future<Either<Failure, List<PeopleEntity?>>> getPeopleByNationalNo({
+    required String nationalNo,
+  });
+  Future<Either<Failure, List<PeopleEntity?>>> getPeopleByFirstName({
+    required String firstName,
+  });
 }
