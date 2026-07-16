0ff0717aa6944007c5325d929fc5276a6a634570
engIbrahemQruea <mmany6186@gmail.com>
2026-07-17 00:57:44 +0300

    feat: implement add and update functionality for people, enhance error handling, and add country retrieval methods

--- apps/dvld/lib/features/people/data/repos_imp/people_repos_imp.dart
+++ apps/dvld/lib/features/people/data/repos_imp/people_repos_imp.dart
@@ -2,6 +2,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:dvld/core/error/failure.dart';
 import 'package:dvld/features/people/data/data_sources/local_data_sources/local_data_source.dart';
+import 'package:dvld/features/people/data/models/people_models.dart';
+import 'package:dvld/features/people/domain/entities/county_entity.dart';
 import 'package:dvld/features/people/domain/entities/people_entity.dart';
 import 'package:dvld/features/people/domain/repos/people_repos.dart';
 
@@ -16,7 +18,7 @@ class PeopleReposImp extends PeopleRepos {
         peopleListModels.map((model) => model.mapToEntity()).toList(),
       );
     } on Exception catch (e) {
-      return Left(Failure());
+      return Left(DatabaseFailure(e.toString()));
     }
   }
 
@@ -36,7 +38,7 @@ class PeopleReposImp extends PeopleRepos {
         return Right([null]);
       }
     } catch (e) {
-      return Left(Failure());
+      return Left(DatabaseFailure(e.toString()));
     }
   }
 
@@ -54,7 +56,7 @@ class PeopleReposImp extends PeopleRepos {
         return Right([null]);
       }
     } catch (e) {
-      return Left(Failure());
+      return Left(DatabaseFailure(e.toString()));
     }
   }
 
@@ -74,7 +76,106 @@ class PeopleReposImp extends PeopleRepos {
         return Right([null]);
       }
     } catch (e) {
-      return Left(Failure());
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> addPeople({
+    required PeopleEntity peopleEntity,
+  }) async {
+    try {
+      final peopleModel = PeopleModels.fromEntity(peopleEntity);
+      final newPeopleId = await _dbHelper.addNewPeople(peopleModel);
+
+      return Right(newPeopleId >= 0);
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> updatePeople({
+    required PeopleEntity peopleEntity,
+  }) async {
+    try {
+      final peopleModel = PeopleModels.fromEntity(peopleEntity);
+      final isUpdated = await _dbHelper.updatePeople(peopleModel);
+      return Right(isUpdated >= 0);
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, PeopleEntity?>> getInfoPeopleById({
+    required int personID,
+  }) async {
+    try {
+      final peopleResult = await _dbHelper.getPeopleById(personID: personID);
+      if (peopleResult.isNotEmpty) {
+        return Right(peopleResult[0]!.mapToEntity());
+      } else {
+        return Right(null);
+      }
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> isNationalNoExists({
+    required String nationalNo,
+  }) async {
+    try {
+      final isExists = await _dbHelper.isNationalNoExists(
+        nationalNo: nationalNo,
+      );
+      return Right(isExists);
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
+
+  /// Country Repo Impl
+
+  @override
+  Future<Either<Failure, List<CountyEntity>>> getAllCountries() async {
+    try {
+      final listModelCountries = await _dbHelper.getAllCountries();
+      return Right(
+        listModelCountries.map((model) => model.mapToEntity()).toList(),
+      );
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, int?>> getCountryIdByName({
+    required String countryName,
+  }) async {
+    try {
+      final countryID = await _dbHelper.getCountryIdByName(
+        countryName: countryName,
+      );
+      return Right(countryID);
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, String?>> getCountryNameById({
+    required int countryId,
+  }) async {
+    try {
+      final countryName = await _dbHelper.getCountryNameById(
+        countryId: countryId,
+      );
+      return Right(countryName);
+    } catch (e) {
+      return Left(DatabaseFailure(e.toString()));
     }
   }
 }
--- apps/dvld/lib/features/people/domain/repos/people_repos.dart
+++ apps/dvld/lib/features/people/domain/repos/people_repos.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:dvld/core/error/failure.dart';
+import 'package:dvld/features/people/domain/entities/county_entity.dart';
 import 'package:dvld/features/people/domain/entities/people_entity.dart';
 
 abstract class PeopleRepos {
@@ -13,4 +14,18 @@ abstract class PeopleRepos {
   Future<Either<Failure, List<PeopleEntity?>>> getPeopleByFirstName({
     required String firstName,
   });
+
+  /// Add Update Screen
+  Future<Either<Failure, bool>> addPeople({required PeopleEntity peopleEntity});
+
+Future<Either<Failure, PeopleEntity?>> getInfoPeopleById({required int personID});
+
+  Future<Either<Failure, bool>> updatePeople({required PeopleEntity peopleEntity});
+
+  Future<Either<Failure, bool>> isNationalNoExists({required String nationalNo});
+
+/// Country Repos
+Future<Either<Failure, List<CountyEntity>>> getAllCountries();
+Future<Either<Failure, String?>> getCountryNameById({required int countryId});
+Future<Either<Failure, int?>> getCountryIdByName({required String countryName});
 }
