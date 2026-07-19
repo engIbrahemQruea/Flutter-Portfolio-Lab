a09d72bc0057f0f5495ac4bc3216b1a765d89646
engIbrahemQruea <mmany6186@gmail.com>
2026-07-20 02:30:41 +0300

    feat: enhance image picker functionality with dynamic image display and update logic

--- apps/dvld/lib/features/people/presentation/screens/sub_screens/widgets_add_update_people_screen/set_remove_image_picker_section/set_remove_image_picker.dart
+++ apps/dvld/lib/features/people/presentation/screens/sub_screens/widgets_add_update_people_screen/set_remove_image_picker_section/set_remove_image_picker.dart
@@ -1,8 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
+
 import 'package:dvld/core/widgets/app_button.dart';
+import 'package:dvld/features/people/presentation/logic/add_pdate_form/add_update_form_cubit.dart';
+import 'package:dvld/features/people/presentation/logic/controllers/add_update_people_form_controllers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SetRemoveImagePicker extends StatelessWidget {
   const SetRemoveImagePicker({super.key});
+
+
+  static final ImagePicker _picker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
@@ -10,21 +20,41 @@ class SetRemoveImagePicker extends StatelessWidget {
       width: 150,
       child: Column(
         children: [
-          Container(
-            width: 130,
-            height: 140,
-            decoration: BoxDecoration(
-              color: Colors.grey[300],
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(color: Colors.grey),
-            ),
-            child: const Icon(Icons.person, size: 50, color: Colors.grey),
+          BlocBuilder<AddUpdateFormCubit, AddUpdateFormState>(
+            builder: (context, state) {
+              return Container(
+                width: 130,
+                height: 140,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.grey),
+                ),
+                child:
+                    state.imagePickerStatus.imagePath == null ||
+                        state.imagePickerStatus.imagePath == ''
+                    ? const Icon(Icons.person, size: 50, color: Colors.grey)
+                    : Image.file(
+                        File(state.imagePickerStatus.imagePath!),
+                        fit: BoxFit.cover,
+                      ),
+              );
+            },
           ),
           const SizedBox(height: 15),
           AppButton.custom(
             label: 'Set Image',
             icon: const Icon(Icons.upload_file),
-            onPressed: () {},
+            onPressed: () async {
+              final image = await _picker.pickImage(
+                source: ImageSource.gallery,
+              );
+              if (image != null) {
+                context.read<AddUpdateFormCubit>().onChangeImagePicker(
+                  imagePath: image.path,
+                );
+              }
+            },
             size: AppButtonSize.small,
           ),
           const SizedBox(height: 8),
