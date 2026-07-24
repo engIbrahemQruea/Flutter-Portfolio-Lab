60db2b8a3fd1ad6c521c2129bc521a8719cfb852
engIbrahemQruea <mmany6186@gmail.com>
2026-07-24 17:14:27 +0300

    add: enhance PersonImage widget to support displaying person's image and edit functionality

--- apps/dvld/lib/features/people/presentation/shared_widgets/person_information_card/widgets/person_image.dart
+++ apps/dvld/lib/features/people/presentation/shared_widgets/person_information_card/widgets/person_image.dart
@@ -1,22 +1,37 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
 import 'package:flutter/material.dart';
 
+import 'package:dvld/features/people/domain/entities/people_entity.dart';
+
 class PersonImage extends StatelessWidget {
-  const PersonImage({super.key});
+  const PersonImage({
+    Key? key,
+    this.person,
+    this.onEdit,
+  }) : super(key: key);
+
+  final PeopleEntity? person;
+
+  final void Function(int personId)? onEdit;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       spacing: 35,
       children: [
-        RichText(
-          text: TextSpan(
-            text: 'Edit Person Info',
-            style: TextStyle(
-              decoration: TextDecoration.underline,
-              color: Colors.blue,
-              fontSize: 16,
-              decorationColor: Colors.red,
-              decorationThickness: 2.0,
+        InkWell(
+          onTap: () => onEdit?.call(person!.personId!),
+          child: RichText(
+            text: TextSpan(
+              text: 'Edit Person Info',
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+                color: Colors.blue,
+                fontSize: 16,
+                decorationColor: Colors.red,
+                decorationThickness: 2.0,
+              ),
             ),
           ),
         ),
@@ -28,18 +43,17 @@ class PersonImage extends StatelessWidget {
             borderRadius: BorderRadius.circular(8),
             border: Border.all(color: Colors.grey),
           ),
-          child: const Icon(Icons.person, size: 50, color: Colors.grey),
-
-          // ? Image.file(
-          //     File(imagePath),
-          //     fit: BoxFit.cover,
-          //     errorBuilder: (context, error, stackTrace) =>
-          //         const Icon(
-          //           Icons.broken_image,
-          //           size: 50,
-          //           color: Colors.grey,
-          //         ),
-          //   )
+          child: person?.imagePath != null
+              ? Image.file(
+                  File(person!.imagePath!),
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) => const Icon(
+                    Icons.broken_image,
+                    size: 50,
+                    color: Colors.grey,
+                  ),
+                )
+              : Icon(Icons.person, size: 50, color: Colors.grey),
         ),
       ],
     );
