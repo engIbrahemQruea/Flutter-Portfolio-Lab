bc838e798e7897ee540757ce68c1b4741eba3793
engIbrahemQruea <mmany6186@gmail.com>
2026-07-24 17:13:45 +0300

    add: enhance PersonAddressInfo widget to display country and address from person entity

--- apps/dvld/lib/features/people/presentation/shared_widgets/person_information_card/widgets/person_address_info.dart
+++ apps/dvld/lib/features/people/presentation/shared_widgets/person_information_card/widgets/person_address_info.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dvld/core/helpers/spacing.dart';
 import 'package:dvld/core/widgets/app_text_field.dart';
+import 'package:dvld/features/people/domain/entities/people_entity.dart';
+import 'package:dvld/features/people/presentation/person_details_screen/logic/person_details_cubit/person_details_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PersonAddressInfo extends StatelessWidget {
-  const PersonAddressInfo({super.key});
+  const PersonAddressInfo({Key? key, this.person}) : super(key: key);
+
+  final PeopleEntity? person;
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +18,8 @@ class PersonAddressInfo extends StatelessWidget {
         Expanded(
           child: AppTextField(
             label: 'Country',
+            initialValue:
+                context.read<PersonDetailsCubit>().countryName ?? '[????]',
             prefixIcon: Icons.location_on,
             isReadOnly: true,
           ),
@@ -20,6 +28,7 @@ class PersonAddressInfo extends StatelessWidget {
         Expanded(
           child: AppTextField(
             label: 'Address',
+            initialValue: person?.address ?? '[????]',
             prefixIcon: Icons.add_reaction,
             isReadOnly: true,
           ),
