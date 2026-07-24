39f9a1a144e24f77c367eb8180c7eaf1d841a483
engIbrahemQruea <mmany6186@gmail.com>
2026-07-24 17:13:33 +0300

    add: update PersonContactInfo widget to display email and phone from person entity

--- apps/dvld/lib/features/people/presentation/shared_widgets/person_information_card/widgets/person_contact_info.dart
+++ apps/dvld/lib/features/people/presentation/shared_widgets/person_information_card/widgets/person_contact_info.dart
@@ -1,9 +1,17 @@
-import 'package:dvld/core/helpers/spacing.dart';
-import 'package:dvld/core/widgets/app_text_field.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import 'package:dvld/core/helpers/spacing.dart';
+import 'package:dvld/core/widgets/app_text_field.dart';
+import 'package:dvld/features/people/domain/entities/people_entity.dart';
+
 class PersonContactInfo extends StatelessWidget {
-  const PersonContactInfo({super.key});
+  const PersonContactInfo({
+    Key? key,
+    this.person,
+  }) : super(key: key);
+
+  final PeopleEntity? person;
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +20,7 @@ class PersonContactInfo extends StatelessWidget {
         Expanded(
           child: AppTextField(
             label: 'Email',
+            initialValue: person?.email ?? '[????]',
             prefixIcon: Icons.email,
             isReadOnly: true,
           ),
@@ -20,6 +29,7 @@ class PersonContactInfo extends StatelessWidget {
         Expanded(
           child: AppTextField(
             label: 'Phone',
+            initialValue: person?.phone ?? '[????]',
             prefixIcon: Icons.phone,
             isReadOnly: true,
           ),
