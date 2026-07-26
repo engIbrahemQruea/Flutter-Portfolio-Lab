61b64b2d9cd36c55b68d49cfd6b8e9717b0ed890
engIbrahemQruea <mmany6186@gmail.com>
2026-07-26 20:53:53 +0300

    add: update routing to include manageUsersScreen and remove users route

--- apps/dvld/lib/core/routing/routes.dart
+++ apps/dvld/lib/core/routing/routes.dart
@@ -2,9 +2,9 @@ abstract class DRoutes {
   static const String dashboard = '/';
   static const String applications = '/applications';
   static const String peopleScreen = '/peopleScreen';
+  static const String manageUsersScreen = '/manageUsersScreen';
   //Sub screen to People Screen
   static const String addUpdatePeopleScreen = '/addUpdatePeopleScreen';
   static const String personDetailsScreen = '/personDetailsScreen';
   static const String drivers = '/drivers';
-  static const String users = '/users';
 }
