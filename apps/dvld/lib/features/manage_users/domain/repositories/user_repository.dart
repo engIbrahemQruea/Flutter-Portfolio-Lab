9af75ce05728417240d400045558bf12c1b98968
engIbrahemQruea <mmany6186@gmail.com>
2026-07-28 21:32:50 +0300

    add: implement UserRepository interface for user management

---
+++ apps/dvld/lib/features/manage_users/domain/repositories/user_repository.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:dvld/core/error/failure.dart';
+import 'package:dvld/features/manage_users/domain/entities/user_entity.dart';
+
+abstract class UserRepository {
+  Future<Either<Failure, List<UserEntity>>> getAllUsers();
+
+  Future<Either<Failure, UserEntity?>> getUserInfoByUserID({required int userID});
+  Future<Either<Failure, UserEntity?>> getUserInfoByPersonID({required int personID});
+  Future<Either<Failure, UserEntity?>> getUserInfoByUserName({required String userName});
+  Future<Either<Failure, UserEntity?>> getUserInfoByPassword({required String password});
+}
