9b874c0d1c43cff4e38893ff6a6e90324b40a9a7
engIbrahemQruea <mmany6186@gmail.com>
2026-08-03 23:02:57 +0300

    refactor: extend UserEntity to implement Equatable for value comparison

--- apps/dvld/lib/features/manage_users/domain/entities/user_entity.dart
+++ apps/dvld/lib/features/manage_users/domain/entities/user_entity.dart
@@ -1,16 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-class UserEntity {
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
   final int? userID;
   final int personID;
   final String userName;
   final String password;
   final bool isActive;
 
-  UserEntity({
+  const UserEntity({
     this.userID,
     required this.personID,
     required this.userName,
     required this.password,
     required this.isActive,
   });
+
+  @override
+  List<Object?> get props => [userID, personID, userName, password, isActive];
 }
