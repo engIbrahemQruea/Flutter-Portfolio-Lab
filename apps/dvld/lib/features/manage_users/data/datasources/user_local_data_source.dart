6dc446b567df040333514ff425361d7c4d27d163
engIbrahemQruea <mmany6186@gmail.com>
2026-08-03 23:02:25 +0300

    add: implement user management methods in UserLocalDataSource

--- apps/dvld/lib/features/manage_users/data/datasources/user_local_data_source.dart
+++ apps/dvld/lib/features/manage_users/data/datasources/user_local_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:dvld/core/database/app_database.dart';
 import 'package:dvld/features/manage_users/data/datasources/user_table.dart';
+import 'package:sqflite/sqflite.dart';
 
 class UserLocalDataSource {
   final AppDatabase appDatabase;
@@ -10,11 +11,9 @@ class UserLocalDataSource {
     return await db.query(UserTable.tableName, distinct: true);
   }
 
-  Future<Map<String, dynamic>?> getUserInfoById({
-    required int userID,
-  }) async {
+  Future<Map<String, dynamic>?> getUserInfoById({required int userID}) async {
     final db = await appDatabase.database;
-    final result= await db.query(
+    final result = await db.query(
       UserTable.tableName,
       where: '${UserTable.colUserId} = ?',
       whereArgs: [userID],
@@ -23,11 +22,11 @@ class UserLocalDataSource {
     return result.firstOrNull;
   }
 
- Future<Map<String, dynamic>?> getUserInfoByPersonID({
+  Future<Map<String, dynamic>?> getUserInfoByPersonID({
     required int personID,
   }) async {
     final db = await appDatabase.database;
-    final result= await db.query(
+    final result = await db.query(
       UserTable.tableName,
       where: '${UserTable.colPersonId} = ?',
       whereArgs: [personID],
@@ -40,7 +39,7 @@ class UserLocalDataSource {
     required String userName,
   }) async {
     final db = await appDatabase.database;
-    final result= await db.query(
+    final result = await db.query(
       UserTable.tableName,
       where: '${UserTable.colUserName} = ?',
       whereArgs: [userName],
@@ -48,15 +47,58 @@ class UserLocalDataSource {
     return result.firstOrNull;
   }
 
- Future<Map<String, dynamic>?> getUserInfoByPassword({
+  Future<Map<String, dynamic>?> getUserInfoByPassword({
     required String password,
   }) async {
     final db = await appDatabase.database;
-    final result= await db.query(
+    final result = await db.query(
       UserTable.tableName,
       where: '${UserTable.colPassword} = ?',
       whereArgs: [password],
     );
     return result.firstOrNull;
+  }
+
+  Future<bool> isUserExistForPersonID({required int personID}) async {
+    final db = await appDatabase.database;
+
+    final result = await db.rawQuery(
+      'SELECT EXISTS(SELECT 1 FROM ${UserTable.tableName} WHERE ${UserTable.colPersonId} = ?)',
+      [personID],
+    );
+
+    return Sqflite.firstIntValue(result) == 1;
+  }
+
+  Future<Map<String, dynamic>?> addNewUser({
+    required Map<String, dynamic> userMap,
+  }) async {
+    final db = await appDatabase.database;
+
+    final id = await db.insert(
+      UserTable.tableName,
+      userMap,
+      conflictAlgorithm: ConflictAlgorithm.abort,
+    );
+
+    return getUserInfoById(userID: id);
+  }
+
+  Future<Map<String, dynamic>?> updateUser({
+    required Map<String, dynamic> userMap,
+  }) async {
+    final db = await appDatabase.database;
+    final id = userMap[UserTable.colUserId];
+    if (id == null) return null;
+
+    final rowsAffected = await db.update(
+      UserTable.tableName,
+      userMap,
+      where: '${UserTable.colUserId} = ?',
+      whereArgs: [id],
+    );
+    if (rowsAffected == 0) return null;
+
+    return getUserInfoById(userID: id);
   }
 }
