6d05abfebad3ace36dc623fa65557eb2ba399673
engIbrahemQruea <mmany6186@gmail.com>
2026-08-04 17:35:26 +0300

    add: create login_info_tab_widgets for user management screen

---
+++ apps/dvld/lib/features/manage_users/presentation/screens/add_update_users_screen/widgets/login_info_tab_widgets/login_info_tab_widgets.dart
@@ -0,0 +1,5 @@
+export 'confirm_password_text_form.dart';
+export 'is_active_check_box.dart';
+export 'password_text_form.dart';
+export 'user_id_text_form.dart';
+export 'user_name_text_form.dart';
