f073d1f05cc0e7d06c2b137f8ce90c92907a1cab
engIbrahemQruea <mmany6186@gmail.com>
2026-08-04 20:58:23 +0300

    add: define route for showDetailsUserScreen in DRoutes

--- apps/dvld/lib/core/routing/routes.dart
+++ apps/dvld/lib/core/routing/routes.dart
@@ -9,5 +9,6 @@ abstract class DRoutes {
 
   //Sub screen to Manage Users Screen
   static const String addUpdateUsersScreen = '/addUpdateUsersScreen';
+  static const String showDetailsUserScreen = '/showDetailsUserScreen';
   static const String drivers = '/drivers';
 }
