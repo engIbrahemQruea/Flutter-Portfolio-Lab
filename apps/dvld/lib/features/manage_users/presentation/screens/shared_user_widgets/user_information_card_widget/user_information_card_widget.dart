19c643d531fffefcb9f81c8f7ab931b16cb437da
engIbrahemQruea <mmany6186@gmail.com>
2026-08-05 22:49:52 +0300

    add: create UserInformationCardWidget for displaying user details and handling state changes

---
+++ apps/dvld/lib/features/manage_users/presentation/screens/shared_user_widgets/user_information_card_widget/user_information_card_widget.dart
@@ -0,0 +1,85 @@
+import 'package:dvld/core/helpers/app_dialogs.dart';
+import 'package:dvld/core/helpers/forms/forms.dart';
+import 'package:dvld/core/helpers/spacing.dart';
+import 'package:dvld/core/routing/routes.dart';
+import 'package:dvld/features/manage_users/presentation/logic/user_information_card_cubit/user_information_card_cubit.dart';
+import 'package:dvld/features/manage_users/presentation/screens/shared_user_widgets/user_information_card_widget/widgets/login_information_user_widget.dart';
+import 'package:dvld/features/people/presentation/shared_widgets/person_information_card/person_information_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+class UserInformationCardWidget extends StatelessWidget {
+  const UserInformationCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<
+      UserInformationCardCubit,
+      UserInformationCardCubitState
+    >(
+      listenWhen: (previous, current) =>
+          previous.userInformationCardStatus !=
+              current.userInformationCardStatus ||
+          previous.peopleEntity != current.peopleEntity,
+      listener: (context, state) {
+        switch (state.userInformationCardStatus) {
+          case RequestStatus.loading:
+            AppDialogs.showLoading(context: context);
+            break;
+
+          case RequestStatus.success:
+            // context.pop();
+            break;
+
+          case RequestStatus.failure:
+            context.pop();
+            AppDialogs.showFailure(
+              context: context,
+              title: 'Error Loading User',
+              buttonText: 'OK',
+              message: state.errorMessage ?? 'Something went wrong',
+            );
+            break;
+
+          case RequestStatus.initial:
+            break;
+        }
+      },
+
+      buildWhen: (previous, current) =>
+          previous.userInformationCardStatus !=
+              current.userInformationCardStatus ||
+          previous.peopleEntity != current.peopleEntity ||
+          previous.countryName != current.countryName,
+
+      builder: (context, state) {
+        return switch (state.userInformationCardStatus) {
+          RequestStatus.success => Column(
+            children: [
+              verticalSpace(5),
+              PersonInformationCard(
+                person: state.peopleEntity,
+                countryName: state.countryName,
+                onEdit: (personId) async {
+                  final result = await context.pushNamed<bool>(
+                    DRoutes.addUpdatePeopleScreen,
+                    queryParameters: {'personId': personId.toString()},
+                  );
+                  if (result == true && context.mounted) {
+                    await context
+                        .read<UserInformationCardCubit>()
+                        .refreshPersonInfo(personID: personId);
+                  }
+                },
+              ),
+              verticalSpace(20),
+              const LoginInformationUserWidget(),
+            ],
+          ),
+          _ => const SizedBox.shrink(),
+        };
+      },
+    );
+  }
+}
