d7760c9e60a5e4d0a6ab2c3501486b2b1f665442
engIbrahemQruea <mmany6186@gmail.com>
2026-08-07 00:31:07 +0300

    add: enhance UserInformationCardWidget to support user selection callback

--- apps/dvld/lib/features/manage_users/presentation/screens/shared_user_widgets/user_information_card_widget/user_information_card_widget.dart
+++ apps/dvld/lib/features/manage_users/presentation/screens/shared_user_widgets/user_information_card_widget/user_information_card_widget.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dvld/core/helpers/app_dialogs.dart';
 import 'package:dvld/core/helpers/forms/forms.dart';
 import 'package:dvld/core/helpers/spacing.dart';
 import 'package:dvld/core/routing/routes.dart';
+import 'package:dvld/features/manage_users/domain/entities/user_entity.dart';
 import 'package:dvld/features/manage_users/presentation/logic/user_information_card_cubit/user_information_card_cubit.dart';
 import 'package:dvld/features/manage_users/presentation/screens/shared_user_widgets/user_information_card_widget/widgets/login_information_user_widget.dart';
 import 'package:dvld/features/people/presentation/shared_widgets/person_information_card/person_information_card.dart';
@@ -10,7 +12,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class UserInformationCardWidget extends StatelessWidget {
-  const UserInformationCardWidget({super.key});
+  const UserInformationCardWidget({super.key, this.onUserSelected});
+
+  final ValueChanged<UserEntity?>? onUserSelected;
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +25,10 @@ class UserInformationCardWidget extends StatelessWidget {
       listenWhen: (previous, current) =>
           previous.userInformationCardStatus !=
               current.userInformationCardStatus ||
-          previous.peopleEntity != current.peopleEntity,
+          previous.peopleEntity != current.peopleEntity ||
+          previous.userEntity != current.userEntity,
       listener: (context, state) {
+        onUserSelected?.call(state.userEntity);
         switch (state.userInformationCardStatus) {
           case RequestStatus.loading:
             AppDialogs.showLoading(context: context);
