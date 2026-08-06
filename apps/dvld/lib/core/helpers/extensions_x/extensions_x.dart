311383a284594c59f56b2644d6e42f88bbc4dce7
engIbrahemQruea <mmany6186@gmail.com>
2026-08-07 00:28:23 +0300

    add: create extensions_x file to consolidate export of various extension modules

---
+++ apps/dvld/lib/core/helpers/extensions_x/extensions_x.dart
@@ -0,0 +1,4 @@
+export 'package:dvld/core/helpers/extensions_x/context_extensions_x.dart';
+export 'package:dvld/core/helpers/extensions_x/date_time_extensions_x.dart';
+export 'package:dvld/core/helpers/extensions_x/string_extensions_x.dart';
+export 'package:dvld/core/helpers/extensions_x/widget_extensions_x.dart';
