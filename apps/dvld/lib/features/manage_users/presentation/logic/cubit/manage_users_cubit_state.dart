f321ffc46e4f486954445d779356d6fcd7f2c31c
engIbrahemQruea <mmany6186@gmail.com>
2026-08-07 23:17:09 +0300

    add: include filteredUsers and selectedFilterIsActiveOption in ManageUsersCubitState

--- apps/dvld/lib/features/manage_users/presentation/logic/cubit/manage_users_cubit_state.dart
+++ apps/dvld/lib/features/manage_users/presentation/logic/cubit/manage_users_cubit_state.dart
@@ -6,39 +6,51 @@ enum EnManageUsersStatus { initial, loading, success, failure }
 class ManageUsersCubitState extends Equatable {
   const ManageUsersCubitState(
     this.users,
+    this.filteredUsers,
     this.usersStatus,
     this.selectedFilterOption,
+    this.selectedFilterIsActiveOption,
     this.searchQuery,
     this.errorMessage,
   );
 
   factory ManageUsersCubitState.initial() => const ManageUsersCubitState(
     [],
+    [],
     EnManageUsersStatus.initial,
     EnUsersFilterOption.none,
+    IsActiveOption.all,
     null,
     null,
   );
 
   final List<UserEntity> users;
+  final List<UserEntity> filteredUsers;
   final EnManageUsersStatus usersStatus;
   final EnUsersFilterOption selectedFilterOption;
+  final IsActiveOption selectedFilterIsActiveOption;
   final String? searchQuery;
   final String? errorMessage;
 
   bool get isFilterAction => selectedFilterOption == EnUsersFilterOption.none;
+  bool get isFilterByIsActive =>
+      selectedFilterOption == EnUsersFilterOption.isActive;
 
   ManageUsersCubitState copyWith({
     List<UserEntity>? users,
+    List<UserEntity>? filteredUsers,
     EnManageUsersStatus? usersStatus,
     EnUsersFilterOption? selectedFilterOption,
+    IsActiveOption? selectedFilterIsActiveOption,
     String? searchQuery,
     String? Function()? errorMessage,
   }) {
     return ManageUsersCubitState(
       users ?? this.users,
+      filteredUsers ?? this.filteredUsers,
       usersStatus ?? this.usersStatus,
       selectedFilterOption ?? this.selectedFilterOption,
+      selectedFilterIsActiveOption ?? this.selectedFilterIsActiveOption,
       searchQuery ?? this.searchQuery,
       errorMessage != null ? errorMessage() : this.errorMessage,
     );
@@ -47,8 +59,10 @@ class ManageUsersCubitState extends Equatable {
   @override
   List<Object?> get props => [
     users,
+    filteredUsers,
     usersStatus,
     selectedFilterOption,
+    selectedFilterIsActiveOption,
     searchQuery,
     errorMessage,
   ];
