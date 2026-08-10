954b477f47d4e390700c1c499abf27bdbc185f00
engIbrahemQruea <mmany6186@gmail.com>
2026-08-10 16:47:21 +0300

    refactor: add shared preferences keys for user authentication

--- apps/dvld/lib/core/helpers/constance.dart
+++ apps/dvld/lib/core/helpers/constance.dart
@@ -1,1 +1,5 @@
 const String destinationFolder = r'D:\Backups\DVLD-People-Images\';
+
+/// Shared Preferences Keys
+const String sharedPrefKeyRememberMe = 'REMEMBER_ME';
+const String sharedPrefKeyCurrentUser = 'CURRENT_USER';
