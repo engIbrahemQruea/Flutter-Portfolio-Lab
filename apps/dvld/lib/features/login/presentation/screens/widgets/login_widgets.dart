df6cc4e8b9b37efc2feefe1b82455fdcb8e83d0a
engIbrahemQruea <mmany6186@gmail.com>
2026-08-10 16:56:49 +0300

    add: create  barrel file login_widgets.dart to export login-related widgets and controllers

---
+++ apps/dvld/lib/features/login/presentation/screens/widgets/login_widgets.dart
@@ -0,0 +1,6 @@
+export 'package:dvld/features/login/presentation/screens/helpers/login_screen_controllers.dart';
+export 'package:dvld/features/login/presentation/screens/widgets/is_remember_me_check_box_widget.dart';
+export 'package:dvld/features/login/presentation/screens/widgets/login_button_widget.dart';
+export 'package:dvld/features/login/presentation/screens/widgets/password_text_form_field_widget.dart';
+export 'package:dvld/features/login/presentation/screens/widgets/user_name_text_form_field_widget.dart';
+export 'package:flutter/material.dart';
