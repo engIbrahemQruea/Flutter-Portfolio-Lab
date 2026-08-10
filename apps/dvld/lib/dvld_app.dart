0ebd1d99fc74174c3dea715d8cbf09918881d10f
engIbrahemQruea <mmany6186@gmail.com>
2026-08-10 16:57:41 +0300

    refactor: wrap MaterialApp.router with AppLifecycleManager for improved app lifecycle management

--- apps/dvld/lib/dvld_app.dart
+++ apps/dvld/lib/dvld_app.dart
@@ -1,4 +1,5 @@
 import 'package:dvld/core/routing/app_router.dart';
+import 'package:dvld/core/widgets/app_lifecycle_manager.dart';
 import 'package:flutter/material.dart';
 
 class DvldApp extends StatelessWidget {
@@ -6,11 +7,13 @@ class DvldApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'DVLD',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      routerConfig: AppRouter.router,
+    return AppLifecycleManager(
+      child: MaterialApp.router(
+        title: 'DVLD',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+        routerConfig: AppRouter.router,
+      ),
     );
   }
 }
