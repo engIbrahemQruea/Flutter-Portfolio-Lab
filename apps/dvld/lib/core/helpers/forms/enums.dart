16acf6dc3438a41a64fc33e4a3ff0e150c1e524e
engIbrahemQruea <mmany6186@gmail.com>
2026-08-14 23:35:11 +0300

    refactor: update RequestStatus enum to improve readability and maintainability

--- apps/dvld/lib/core/helpers/forms/enums.dart
+++ apps/dvld/lib/core/helpers/forms/enums.dart
@@ -1,3 +1,12 @@
 enum ScreenStatus { add, update }
 
-enum RequestStatus { initial, loading, success, failure }
+enum RequestStatus {
+  initial,
+  loading,
+  success,
+  failure;
+
+  bool get isLoading => this == RequestStatus.loading;
+  bool get isSuccess => this == RequestStatus.success;
+  bool get isFailure => this == RequestStatus.failure;
+}
