2fbd21a42b65aa9e2c4421334f58578a4c36f055
engIbrahemQruea <mmany6186@gmail.com>
2026-08-16 00:50:10 +0300

    add: create index.dart for managing test types exports

---
+++ apps/dvld/lib/features/manage_application/test_types/index.dart
@@ -0,0 +1,3 @@
+export 'package:dvld/features/manage_application/test_types/data/index_data_test_type.dart';
+export 'package:dvld/features/manage_application/test_types/domain/index_domain_test_type.dart';
+export 'package:dvld/features/manage_application/test_types/ui/index.dart';
