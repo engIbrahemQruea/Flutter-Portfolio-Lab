458640805f673f93e5ad7e6f5c76431f004282f6
engIbrahemQruea <mmany6186@gmail.com>
2026-08-15 15:34:12 +0300

    add: implement ApplicationTypesScreenCubit for managing application types state

---
+++ apps/dvld/lib/features/manage_application/application_types/ui/logic/application_types_screen_cubit/application_types_screen_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:dvld/features/manage_application/application_types/domain/index_domain_application_type.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'application_types_screen_cubit_state.dart';
+
+class ApplicationTypesScreenCubit
+    extends Cubit<ApplicationTypesScreenCubitState> {
+  ApplicationTypesScreenCubit(this._getAllApplicationTypesUseCase)
+    : super(ApplicationTypesScreenCubitInitial());
+
+  final GetAllApplicationTypesUseCase _getAllApplicationTypesUseCase;
+
+  Future<void> getAllApplicationTypes() async {
+    emit(ApplicationTypesScreenCubitLoading());
+    final result = await _getAllApplicationTypesUseCase.call();
+    result.fold(
+      (l) => emit(ApplicationTypesScreenCubitFailure(errorMessage: l.message)),
+      (r) => emit(ApplicationTypesScreenCubitSuccess(applicationTypesList: r)),
+    );
+  }
+}
