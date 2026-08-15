af84b43e2359c8a6bf1d467cdfb21df745de1ec7
engIbrahemQruea <mmany6186@gmail.com>
2026-08-16 00:46:25 +0300

    add: create inputs.dart to export test type input components for update functionality

---
+++ apps/dvld/lib/features/manage_application/test_types/ui/logic/update_test_types_screen_cubit/inputs/inputs.dart
@@ -0,0 +1,4 @@
+export 'package:dvld/features/manage_application/test_types/ui/logic/update_test_types_screen_cubit/inputs/test_type_description_input.dart';
+export 'package:dvld/features/manage_application/test_types/ui/logic/update_test_types_screen_cubit/inputs/test_type_fees_input.dart';
+export 'package:dvld/features/manage_application/test_types/ui/logic/update_test_types_screen_cubit/inputs/test_type_title_input.dart';
+export 'package:formz/formz.dart';
