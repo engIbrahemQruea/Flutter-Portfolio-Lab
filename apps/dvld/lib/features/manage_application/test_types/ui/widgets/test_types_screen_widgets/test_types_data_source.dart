a18acf296b7d52881c7dd4a3151adf3bbbb1b13e
engIbrahemQruea <mmany6186@gmail.com>
2026-08-15 21:36:31 +0300

    add: implement TestTypesDataSource for managing test types in data grid

---
+++ apps/dvld/lib/features/manage_application/test_types/ui/widgets/test_types_screen_widgets/test_types_data_source.dart
@@ -0,0 +1,57 @@
+import 'package:dvld/features/manage_application/test_types/domain/entities/test_type_entity.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+
+class TestTypesDataSource extends DataGridSource {
+  TestTypesDataSource({required List<TestTypeEntity> testTypes}) {
+    dataGridRows = testTypes
+        .map<DataGridRow>(
+          (dataGridRow) => DataGridRow(
+            cells: [
+              DataGridCell<int>(
+                columnName: 'test_type_id',
+                value: dataGridRow.testTypeId,
+              ),
+              DataGridCell<String>(
+                columnName: 'test_type_title',
+                value: dataGridRow.testTypeTitle,
+              ),
+              DataGridCell<String>(
+                columnName: 'test_type_description',
+                value: dataGridRow.testTypeDescription,
+              ),
+              DataGridCell<double>(
+                columnName: 'test_type_fee',
+                value: dataGridRow.testTypeFees,
+              ),
+            ],
+          ),
+        )
+        .toList();
+  }
+
+  List<DataGridRow> dataGridRows = [];
+
+  @override
+  List<DataGridRow> get rows => dataGridRows;
+
+  @override
+  DataGridRowAdapter? buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+      cells: row.getCells().map<Widget>((dataGridCell) {
+        return Container(
+          alignment:
+              (dataGridCell.columnName == 'test_type_id' ||
+                  dataGridCell.columnName == 'test_type_fee')
+              ? Alignment.center
+              : Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            dataGridCell.value.toString(),
+            overflow: TextOverflow.ellipsis,
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
