4367d715164f14a4614e44daef62a99bad5afb8a
engIbrahemQruea <mmany6186@gmail.com>
2026-08-15 15:29:56 +0300

    add: create index file for application types exports

---
+++ apps/dvld/lib/features/manage_application/application_types/index_application_types.dart
@@ -0,0 +1,3 @@
+export 'package:dvld/features/manage_application/application_types/data/index_data_application_type.dart';
+export 'package:dvld/features/manage_application/application_types/domain/index_domain_application_type.dart';
+export 'package:dvld/features/manage_application/application_types/ui/index_ui_application_types_screen.dart';
