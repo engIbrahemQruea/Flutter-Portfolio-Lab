413ac3e192f0bf022b9e8f097d1e5631d1363b22
engIbrahemQruea <mmany6186@gmail.com>
2026-08-15 21:34:41 +0300

    add: create TestTypeModel for mapping test type data to entity

---
+++ apps/dvld/lib/features/manage_application/test_types/data/models/test_type_model.dart
@@ -0,0 +1,58 @@
+import 'package:dvld/core/helpers/type_def.dart';
+import 'package:dvld/core/mapper_to_entity/data_mapper.dart';
+import 'package:dvld/features/manage_application/test_types/data/data_sources/test_type_table.dart';
+import 'package:dvld/features/manage_application/test_types/domain/entities/test_type_entity.dart';
+import 'package:equatable/equatable.dart';
+
+class TestTypeModel extends DataMapper<TestTypeEntity> with Equatable {
+  const TestTypeModel({
+    required this.testTypeId,
+    required this.testTypeTitle,
+    required this.testTypeDescription,
+    required this.testTypeFees,
+  });
+
+  final int? testTypeId;
+  final String testTypeTitle;
+  final String testTypeDescription;
+  final double testTypeFees;
+
+  factory TestTypeModel.fromMap(Map<String, dynamic> map) {
+    return TestTypeModel(
+      testTypeId: map[TestTypeTable.colId],
+      testTypeTitle: map[TestTypeTable.colTitle],
+      testTypeDescription: map[TestTypeTable.colDescription],
+      testTypeFees: (map[TestTypeTable.colFees] as num).toDouble(),
+    );
+  }
+
+  JsonMap toMap() => {
+    if (testTypeId != null) TestTypeTable.colId: testTypeId,
+    TestTypeTable.colTitle: testTypeTitle,
+    TestTypeTable.colDescription: testTypeDescription,
+    TestTypeTable.colFees: testTypeFees,
+  };
+
+  factory TestTypeModel.fromEntity(TestTypeEntity entity) => TestTypeModel(
+    testTypeId: entity.testTypeId,
+    testTypeTitle: entity.testTypeTitle,
+    testTypeDescription: entity.testTypeDescription,
+    testTypeFees: entity.testTypeFees,
+  );
+
+  @override
+  TestTypeEntity mapToEntity() => TestTypeEntity(
+    testTypeId: testTypeId,
+    testTypeTitle: testTypeTitle,
+    testTypeDescription: testTypeDescription,
+    testTypeFees: testTypeFees,
+  );
+
+  @override
+  List<Object?> get props => [
+    testTypeId,
+    testTypeTitle,
+    testTypeDescription,
+    testTypeFees,
+  ];
+}
