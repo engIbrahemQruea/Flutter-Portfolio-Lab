2cc7f0ce5e672a48f5594f0aa25245e513d94976
engIbrahemQruea <mmany6186@gmail.com>
2026-08-15 15:36:08 +0300

    add: implement AppTypeIdTextFieldWidget for displaying auto-generated application type ID

---
+++ apps/dvld/lib/features/manage_application/application_types/ui/widgets/update_application_types_screen_widgets/app_type_Id_text_field_widget.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dvld/core/widgets/app_text_field.dart';
+import 'package:dvld/features/manage_application/application_types/ui/logic/update_application_types_screen_cubit/update_application_types_screen_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppTypeIdTextFieldWidget extends StatelessWidget {
+  const AppTypeIdTextFieldWidget({
+    super.key,
+    required this.appTypeIdController,
+  });
+
+  final TextEditingController appTypeIdController;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<
+      UpdateApplicationTypesScreenCubit,
+      UpdateApplicationTypesScreenCubitState
+    >(
+      buildWhen: (previous, current) =>
+          previous.applicationTypeId != current.applicationTypeId,
+      builder: (context, state) {
+        return SizedBox(
+          width: 250,
+          child: AppTextField(
+         //   initialValue: state.applicationTypeId.toString(),
+         controller: appTypeIdController,
+            label: 'App Type ID',
+            hintText: 'Auto Generated',
+            keyboardType: TextInputType.number,
+            isReadOnly: true,
+            prefixIcon: Icons.app_shortcut_outlined,
+          ),
+        );
+      },
+    );
+  }
+}
