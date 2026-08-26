e62a7f2e5a5f753151556cb51a9f43be5b79656d
engIbrahemQruea <mmany6186@gmail.com>
2026-08-26 16:55:08 +0300

    add: implement SetCompleteApplicationUseCase for updating application status to completed

---
+++ apps/dvld/lib/features/applications/applications_core/domain/use_cases/set_complete_application_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:dvld/core/base_use_case/base_use_case.dart';
+import 'package:dvld/core/error/failure.dart';
+import 'package:dvld/features/applications/applications_core/domain/repositories/applications_repository.dart';
+import 'package:dvld/features/applications/applications_core/domain/entities/application_status.dart';
+
+class SetCompleteApplicationUseCase extends BaseUseCase<bool, int> {
+  SetCompleteApplicationUseCase(this._applicationsRepository);
+
+  final ApplicationsRepository _applicationsRepository;
+
+  @override
+  Future<Either<Failure, bool>> call([int? applicationId]) async {
+    return _applicationsRepository.updateApplicationStatus(
+      applicationId: applicationId!,
+      newStatus: ApplicationStatus.completed.value,
+    );
+  }
+}
