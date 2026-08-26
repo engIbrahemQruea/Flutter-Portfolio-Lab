b8b79ce4b6597ee8cc69c85f67e0a74828f8af65
engIbrahemQruea <mmany6186@gmail.com>
2026-08-26 16:54:09 +0300

    add: implement GetActiveApplicationIDUseCase for retrieving active application IDs

---
+++ apps/dvld/lib/features/applications/applications_core/domain/use_cases/get_active_application_id_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:dvld/core/base_use_case/base_use_case.dart';
+import 'package:dvld/core/error/failure.dart';
+import 'package:dvld/features/applications/applications_core/domain/repositories/applications_repository.dart';
+
+typedef ActiveParams = ({int applicationTypeId, int personId});
+
+class GetActiveApplicationIDUseCase extends BaseUseCase<int, ActiveParams> {
+  final ApplicationsRepository _applicationsRepository;
+  GetActiveApplicationIDUseCase(this._applicationsRepository);
+  @override
+  Future<Either<Failure, int>> call([ActiveParams? params]) async {
+    return _applicationsRepository.getActiveApplicationsID(
+      applicationTypeId: params!.applicationTypeId,
+      personId: params.personId,
+    );
+  }
+}
