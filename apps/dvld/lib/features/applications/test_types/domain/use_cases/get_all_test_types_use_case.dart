6e0f8842d509fb7b0f952620ac6d1b063c854533
engIbrahemQruea <mmany6186@gmail.com>
2026-09-03 17:21:46 +0300

    refactor: update GetAllTestTypesUseCase to use BaseUseCaseNoParams for consistency

--- apps/dvld/lib/features/applications/test_types/domain/use_cases/get_all_test_types_use_case.dart
+++ apps/dvld/lib/features/applications/test_types/domain/use_cases/get_all_test_types_use_case.dart
@@ -1,14 +1,15 @@
-
 import 'package:dvld/features/applications/test_types/domain/index_domain_test_type.dart';
 
 class GetAllTestTypesUseCase
-    extends BaseUseCase<List<TestTypeEntity>, NoParams> {
+    extends BaseUseCase<List<TestTypeEntity>, BaseUseCaseNoParams> {
   GetAllTestTypesUseCase(this._testTypesRepository);
 
   final TestTypesRepository _testTypesRepository;
 
   @override
-  Future<Either<Failure, List<TestTypeEntity>>> call([NoParams? params]) {
+  Future<Either<Failure, List<TestTypeEntity>>> call([
+    BaseUseCaseNoParams? params,
+  ]) {
     return _testTypesRepository.getAllTestTypes();
   }
 }
