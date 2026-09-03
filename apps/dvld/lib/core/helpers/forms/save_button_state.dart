e8c9a6907758b2a72ed666434b06c58e6f95b06c
engIbrahemQruea <mmany6186@gmail.com>
2026-09-03 17:13:35 +0300

    feat: update SaveButtonState constructor to set default saveStatus and enhance state management

--- apps/dvld/lib/core/helpers/forms/save_button_state.dart
+++ apps/dvld/lib/core/helpers/forms/save_button_state.dart
@@ -1,13 +1,27 @@
 import 'package:dvld/core/helpers/forms/enums.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class SaveButtonState extends Equatable {
   final RequestStatus saveStatus;
   final String? errorMessage;
 
-  const SaveButtonState({required this.saveStatus, this.errorMessage});
+  const SaveButtonState({
+    this.saveStatus = RequestStatus.initial,
+    this.errorMessage,
+  });
 
+  const SaveButtonState.initial() : this(saveStatus: RequestStatus.initial);
+
+  const SaveButtonState.loading() : this(saveStatus: RequestStatus.loading);
+
+  const SaveButtonState.success() : this(saveStatus: RequestStatus.success);
+
+  const SaveButtonState.failure([String? errorMessage])
+    : this(saveStatus: RequestStatus.failure, errorMessage: errorMessage);
+
+  // Getters
+  bool get isInitial => saveStatus == RequestStatus.initial;
   bool get isLoading => saveStatus == RequestStatus.loading;
   bool get isSuccess => saveStatus == RequestStatus.success;
   bool get isFailure => saveStatus == RequestStatus.failure;
@@ -25,3 +39,31 @@ class SaveButtonState extends Equatable {
   @override
   List<Object?> get props => [saveStatus, errorMessage];
 }
+
+// import 'package:dvld/core/helpers/forms/enums.dart';
+// import 'package:equatable/equatable.dart';
+// import 'package:flutter/material.dart';
+
+// class SaveButtonState extends Equatable {
+//   final RequestStatus saveStatus;
+//   final String? errorMessage;
+
+//   const SaveButtonState({required this.saveStatus, this.errorMessage});
+
+//   bool get isLoading => saveStatus == RequestStatus.loading;
+//   bool get isSuccess => saveStatus == RequestStatus.success;
+//   bool get isFailure => saveStatus == RequestStatus.failure;
+
+//   SaveButtonState copyWith({
+//     RequestStatus? saveStatus,
+//     ValueGetter<String?>? errorMessage,
+//   }) {
+//     return SaveButtonState(
+//       saveStatus: saveStatus ?? this.saveStatus,
+//       errorMessage: errorMessage != null ? errorMessage() : this.errorMessage,
+//     );
+//   }
+
+//   @override
+//   List<Object?> get props => [saveStatus, errorMessage];
+// }
