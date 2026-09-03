bc9125c924c2d022dce92552fddf38241da89663
engIbrahemQruea <mmany6186@gmail.com>
2026-09-03 17:15:24 +0300

    refactor: update GetListPeopleUseCase to use BaseUseCaseNoParams for improved parameter handling

--- apps/dvld/lib/features/people/domain/usecases/get_list_people_use_case.dart
+++ apps/dvld/lib/features/people/domain/usecases/get_list_people_use_case.dart
@@ -4,12 +4,15 @@ import 'package:dvld/core/error/failure.dart';
 import 'package:dvld/features/people/domain/entities/people_entity.dart';
 import 'package:dvld/features/people/domain/repos/people_repos.dart';
 
-class GetListPeopleUseCase extends BaseUseCase<List<PeopleEntity>, NoParams> {
+class GetListPeopleUseCase
+    extends BaseUseCase<List<PeopleEntity>, BaseUseCaseNoParams> {
   final PeopleRepos _repos;
 
   GetListPeopleUseCase(this._repos);
   @override
-  Future<Either<Failure, List<PeopleEntity>>> call([NoParams? params]) async {
+  Future<Either<Failure, List<PeopleEntity>>> call([
+    BaseUseCaseNoParams? params,
+  ]) async {
     return await _repos.getListPeople();
   }
 }
