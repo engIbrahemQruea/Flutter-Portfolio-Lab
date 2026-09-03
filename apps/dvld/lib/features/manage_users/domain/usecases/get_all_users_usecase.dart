b56bfbdf990779d839dd11bce91955d7698c5b21
engIbrahemQruea <mmany6186@gmail.com>
2026-09-03 17:15:06 +0300

    refactor: update GetAllUsersUseCase to use BaseUseCaseNoParams for improved parameter handling

--- apps/dvld/lib/features/manage_users/domain/usecases/get_all_users_usecase.dart
+++ apps/dvld/lib/features/manage_users/domain/usecases/get_all_users_usecase.dart
@@ -4,13 +4,16 @@ import 'package:dvld/core/error/failure.dart';
 import 'package:dvld/features/manage_users/domain/entities/user_entity.dart';
 import 'package:dvld/features/manage_users/domain/repositories/user_repository.dart';
 
-class GetAllUsersUseCase extends BaseUseCase<List<UserEntity>, NoParams> {
+class GetAllUsersUseCase
+    extends BaseUseCase<List<UserEntity>, BaseUseCaseNoParams> {
   GetAllUsersUseCase(this._userRepository);
 
   final UserRepository _userRepository;
 
   @override
-  Future<Either<Failure, List<UserEntity>>> call([NoParams? params]) async {
+  Future<Either<Failure, List<UserEntity>>> call([
+    BaseUseCaseNoParams? params,
+  ]) async {
     return await _userRepository.getAllUsers();
   }
 }
