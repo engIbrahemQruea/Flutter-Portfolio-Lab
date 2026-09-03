2a9a3e52fad73b183c47305fd1d9f2ee3cb11bfe
engIbrahemQruea <mmany6186@gmail.com>
2026-09-03 17:16:02 +0300

    refactor: update GetAllApplicationTypesUseCase to use BaseUseCaseNoParams for consistency

--- apps/dvld/lib/features/applications/application_types/domain/use_cases/get_all_application_types_use_case.dart
+++ apps/dvld/lib/features/applications/application_types/domain/use_cases/get_all_application_types_use_case.dart
@@ -1,14 +1,14 @@
 import 'package:dvld/features/applications/application_types/domain/index_domain_application_type.dart';
 
 class GetAllApplicationTypesUseCase
-    extends BaseUseCase<List<ApplicationTypeEntity>, NoParams> {
+    extends BaseUseCase<List<ApplicationTypeEntity>, BaseUseCaseNoParams> {
   GetAllApplicationTypesUseCase(this._applicationTypesRepository);
 
   final ApplicationTypesRepository _applicationTypesRepository;
 
   @override
   Future<Either<Failure, List<ApplicationTypeEntity>>> call([
-    NoParams? params,
+    BaseUseCaseNoParams? params,
   ]) async {
     return await _applicationTypesRepository.getAllApplicationTypes();
   }
