8a955bee808924e170e777ffc9d7acb10829b000
engIbrahemQruea <mmany6186@gmail.com>
2026-09-03 17:09:28 +0300

    fix: update BaseUseCase to require Params in call method

--- apps/dvld/lib/core/base_use_case/base_use_case.dart
+++ apps/dvld/lib/core/base_use_case/base_use_case.dart
@@ -2,7 +2,9 @@ import 'package:dartz/dartz.dart';
 import 'package:dvld/core/error/failure.dart';
 
 abstract class BaseUseCase<Type, Params> {
-  Future<Either<Failure, Type>> call([Params params]);
+  Future<Either<Failure, Type>> call(Params params);
 }
 
-class NoParams {}
+abstract class BaseUseCaseNoParams<Type> {
+  Future<Either<Failure, Type>> call();
+}
